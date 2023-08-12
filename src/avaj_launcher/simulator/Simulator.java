a22037603849249a04e76c58ea3717859fb8dccc
etachott <bfwaend@gmail.com>
2023-08-12 20:38:35 -0300

    feat: add simple file IO

--- src/avaj_launcher/simulator/Simulator.java
+++ src/avaj_launcher/simulator/Simulator.java
@@ -1,7 +1,19 @@
 package src.avaj_launcher.simulator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Simulator {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+	public static void main(String[] args) throws Exception {
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
+			System.out.println(bufferedReader.readLine());
+			System.out.println(bufferedReader.readLine());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("No input file provided!");
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't open provided file!");
+		}
 	}
 }

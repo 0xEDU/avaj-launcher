b0109949da2182d17138c25f9292b1912ef09f56
edu <bfwaend@gmail.com>
2023-08-16 01:28:31 -0300

    fix: Coordinates constructor is now public

--- src/avaj_launcher/simulator/weather/Coordinates.java
+++ src/avaj_launcher/simulator/weather/Coordinates.java
@@ -5,7 +5,7 @@ public class Coordinates {
 	private int latitude;
 	private int height;
 	
-	Coordinates(int p_longitude, int p_latitude, int p_height) {
+	public Coordinates(int p_longitude, int p_latitude, int p_height) {
 		this.longitude = p_longitude;
 		this.latitude = p_latitude;
 		this.height = p_height;

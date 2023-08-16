528d784211d1b08ca67862c86f496b09ee913ec3
edu <bfwaend@gmail.com>
2023-08-16 01:29:07 -0300

    feat: add log message to Tower

--- src/avaj_launcher/simulator/Tower.java
+++ src/avaj_launcher/simulator/Tower.java
@@ -2,6 +2,7 @@ package src.avaj_launcher.simulator;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import src.avaj_launcher.simulator.aircraft.Flyable;
 
 public class Tower {
@@ -9,6 +10,7 @@ public class Tower {
 	
 	public void register(Flyable p_flyable) {
 		observers.add(p_flyable);
+		System.out.println("Tower says: " + p_flyable.getName() + " registered to weather tower.");
 	}
 
 	public void unregister(Flyable p_flyable) {

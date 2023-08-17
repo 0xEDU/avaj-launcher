0f179d77e51d901725d9df118c60ae4146d9149c
edu <bfwaend@gmail.com>
2023-08-16 22:37:02 -0300

    feat: add Weather generation to the Towers and apply it to all Aircraft

--- src/avaj_launcher/simulator/Tower.java
+++ src/avaj_launcher/simulator/Tower.java
@@ -2,11 +2,12 @@ package src.avaj_launcher.simulator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import src.avaj_launcher.simulator.aircraft.Flyable;
 
 public class Tower {
-	private List<Flyable> observers = new ArrayList<>();
+	private List<Flyable> observers = new CopyOnWriteArrayList<>();
 	
 	public void register(Flyable p_flyable) {
 		observers.add(p_flyable);
@@ -15,8 +16,12 @@ public class Tower {
 
 	public void unregister(Flyable p_flyable) {
 		observers.remove(p_flyable);
+		System.out.println("Tower says: " + p_flyable.getName() + " unregistered from weather tower.");
 	}
 	
 	protected void conditionChanged() {
+		for (Flyable f : observers) {
+			f.updateConditions();
+		}
 	}
 }
--- src/avaj_launcher/simulator/WeatherTower.java
+++ src/avaj_launcher/simulator/WeatherTower.java
@@ -1,12 +1,14 @@
 package src.avaj_launcher.simulator;
 
 import src.avaj_launcher.simulator.weather.Coordinates;
+import src.avaj_launcher.simulator.weather.WeatherProvider;
 
 public class WeatherTower extends Tower {
 	public String getWeather(Coordinates p_coordinates) {
-		return "oi";
+		return WeatherProvider.getCurrentWeather(p_coordinates);
 	}
 
 	public void changeWeather() {
+		super.conditionChanged();
 	}
 }

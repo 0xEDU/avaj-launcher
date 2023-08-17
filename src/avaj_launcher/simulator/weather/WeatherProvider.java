16e4c37c627c5fb7e379db8f467f878572a0cac4
edu <bfwaend@gmail.com>
2023-08-16 22:35:57 -0300

    feat: add WeatherProvider class

--- src/avaj_launcher/simulator/weather/WeatherProvider.java
+++ src/avaj_launcher/simulator/weather/WeatherProvider.java
@@ -1,18 +1,27 @@
 package src.avaj_launcher.simulator.weather;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.plaf.synth.SynthStyle;
+
 public class WeatherProvider {
 	private static WeatherProvider instance = null;
 	
-	private String[] weather;
+	private static String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
 	
-	private WeatherProvider() {
-	}
+	private WeatherProvider() {}
 	
-	public String getCurrentWeather(Coordinates p_coordinates) {
+	public static String getCurrentWeather(Coordinates p_coordinates) {
 		if (instance == null) {
 			instance = new WeatherProvider();
 		}
-		// Dunno what to do here
-		return instance.weather[0];
+		long seed = System.currentTimeMillis()
+			+ ((p_coordinates.getHeight()
+				+ p_coordinates.getLatitude()
+				+ p_coordinates.getLongitude()) / 3);
+		int random = ThreadLocalRandom.current().nextInt(1, 4 + 1);
+		String current = weather[(int) Math.abs((random + seed )% 4)];
+		return current;
 	}
 }
